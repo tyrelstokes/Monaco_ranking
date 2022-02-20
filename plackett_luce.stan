b309da1623c8e3f36aec345e104ff585be4e3489
tyrelstokes <tyrel.stokes@mail.mcgill.ca>
2022-02-20 02:03:37 -0500

    rmd added

--- plackett_luce.stan
+++ plackett_luce.stan
@@ -14,14 +14,93 @@ functions{
   return out;
   
 }
+
+ int[] ranker(real[] beta,int N){
+   int out[N];
+   int med = 0;
+   real b1;
+   real b2;
+   for(i in 1:N){
+     med = 1;
+     b1 = beta[i];
+     if(i!=1){
+     for(j in 1:(i-1)){
+       b2 = beta[j];
+       if(b1<b2){
+         med += 1;
+       }
+       
+     }
+     }
+     if(i != N){
+     for(k in (i+1):N){
+       b2 = beta[k];
+       if(b1<b2){
+         med += 1;
+       }
+     }
+     
+     }
+     out[i] = med;
+     
+     
+   }
+   return out;
+   
+ }
+ 
+ vector betas_remain(int R,vector betas, int[] excluded_ints, int n_rank, real sigma){
+   
+   vector[R] out;
+   real denom;
+   denom = 0.0;
+   out = exp(betas/sigma);
+   
+   for(i in 1:(n_rank-1)){
+     out[excluded_ints[i]] = 0.0;
+   }
+   
+
+   out = out/sum(out);
+   
+   return out;
+   
+ }
+ 
+ int[] luce_rng(int R, vector betas, real sigma){
+   
+   vector[R] probs;
+   int win_integer[R];
+   int out[R];
+   
+     for(i in 1:R){
+    if(i!=1){
+    probs = betas_remain(8,betas, win_integer[1:(i-1)],i,sigma);
+    }else{
+      probs = softmax(betas/sigma);
+    }
+   win_integer[i] =  categorical_rng(probs);
+   out[win_integer[i]] = i;
+  }
+  
+  
+  return out; 
+   
+   
+   
+ }
+
+
 }
+
 data {
   int<lower=1> N; //the number of observations
   int<lower=1> P;// number of participants
   int x[N,8];// player id matrix. first column is first, second got second etc
-  int nps[N];
-  int n_types;// number of race types -e.g heats, semis, finals
-  int type[N];
+  int nps[N];// number of participants in the race (7-8 in this data set)
+  int n_types;// number of race types -e.g heats, semis, finals )
+  int type[N]; // (ordered from most important to least - i.e finals =1, semis =2, heats =2)
+  int finals[8]; // The integers of the athletes in the finals (you could supply any 8 athletes to simulate a race with those participants if desired)
 }
 
 // The parameters accepted by the model. Our model
@@ -40,19 +119,29 @@ for(j in 2:n_types){
 }
   
 }
-model {
+model{
   
 
  
  for(i in 1:N){
-//y[i,:] ~ luce_lmpf(beta);
-
 target += luce_lpmf(x[i,:]|beta,sigma[type[i]],nps[i]);
 }
   
- 
  beta ~ normal(0,3);
  sig_0 ~ normal(0, 0.5);
  
+}generated quantities{
+  
+ int posterior_latent_ranks[P];
+
+ int replay_ranking[8];
+
+ 
+ posterior_latent_ranks = ranker(to_array_1d(beta),P);
+ 
+ replay_ranking = luce_rng(8,beta[finals],1);
+  
+
+  
 }
 
--- run_model.R
+++ run_model.R
@@ -18,6 +18,7 @@ rank_data <- race_list[[2]]
 
 athletes <- race_list[[3]]
 
+fints <- stan_dt$finals
 
 #########################
 
@@ -26,7 +27,118 @@ library(cmdstanr)
 mod <- cmdstan_model("plackett_luce.stan")
 
 
-fit <- mod$sample(data = stan_dt,iter_warmup = 1200, iter_sampling = 1200,parallel_chains = 4)
+fit <- mod$sample(data = stan_dt,iter_warmup = 2000, iter_sampling = 2000,parallel_chains = 4)
 
 
-fit$cmdstan_summary()
+#############################
+################################
+
+### Collect information and make some plots
+
+
+cv <- fit$summary()
+
+
+chc2 <- fit$summary("posterior_latent_ranks")
+
+post_data <- data.frame(Name = athletes,rank = chc2$mean, Rl = chc2$q5,
+                        Ru = chc2$q95)
+
+fdata <- post_data[fints,] ## Rank information for just those in the finals
+
+
+##########################################
+#########################################
+
+### This gets you the finals data
+
+finals_draws <- fit$draws("replay_ranking", format = "df")
+
+# Reformat this to work well with ggplot2
+library(foreach)
+
+fdata <- foreach(i = 1:8, .combine = rbind)%do%{
+  
+ out <-  data.frame(Name = athletes[fints[i]], Rank = as.vector(finals_draws[,i]))
+  
+  names(out)[2] <- "Rank"
+  out
+}
+
+
+library(ggplot2)
+library(ggthemes)
+
+ggplot(fdata,aes(x= Name,y= Rank))+ geom_boxplot(aes(color = Name)) + 
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  labs(title="Estimated Ranks", 
+       subtitle="Replaying the Olympic 2012 Finals",
+       x="Finalist Name",
+       y="Rank") + coord_flip() +theme_fivethirtyeight()
+
+#############################################
+################################################
+
+## This gets you the ranking plot amoung all participants
+overall_rank_draws <- fit$draws("posterior_latent_ranks", format = "df")
+
+
+odata <- foreach(i = 1:8, .combine = rbind)%do%{
+  
+  out <-  data.frame(Name = athletes[fints[i]], Rank = as.vector(overall_rank_draws[,fints[i]]))
+  
+  names(out)[2] <- "Rank"
+  out
+}
+
+
+ggplot(odata,aes(x= Name,y= Rank))+ geom_boxplot(aes(color = Name)) + 
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  labs(title="Estimated Ranks", 
+       subtitle=" Amoung 87 Participants in WC2011 + 2012 Olympics",
+       x="Finalist Name",
+       y="Rank") + coord_flip() +theme_fivethirtyeight()+scale_y_continuous(breaks = c(0,10,20,30,40,50,60,70,80,90))
+
+
+#######################################################
+#########################################################
+## Finals heat map rank
+library(janitor)
+dd <- tabyl(fdata,Name, Rank)
+
+rtab <- dd
+rtab[,2:9] <- rtab[,2:9]/(nrow(fdata)/8)
+
+rtab2 <- data.frame(Real_Result = c(8,6,3,7,5,4,1,2),rtab[,1:9])
+names(rtab2)[3:ncol(rtab2)] <- c(1:8)
+
+rtab2$Medal <- apply(rtab2[,3:5],1,sum)
+
+names(rtab2)[1] <- "Actual Result"
+
+library(gt)
+library(scales)
+library(readr)
+library(dplyr)
+
+rtab2 %>% arrange(`Actual Result`) %>% mutate_if(is.numeric,~round(.,2))%>% gt() %>% 
+  tab_spanner(label = "Predicted Ranks",
+              columns = c(3:10) )%>%
+  data_color(columns = 3:10,
+             colors = col_numeric(palette = c("white","firebrick"),
+                                  domain = c(0,1))) %>%
+  data_color(columns = 11,
+             colors = col_numeric(palette = c("white","gold"),
+                                  domain = c(0,1)))
+
+
+
+
+
+
+
+
+
+
+
+
